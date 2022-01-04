a0fb0d4ee931ea978f987f0627ef5b56adfb92b5
Jeric Jan Jan <38642145+jericjan@users.noreply.github.com>
2022-01-04 01:37:34 +0000

    keep_alive checks if alr running

--- keep_alive.py
+++ keep_alive.py
@@ -1,6 +1,7 @@
-from flask import Flask, cli
+from flask import Flask, cli, request
 from threading import Thread
 import sys
+import requests
 cli.show_server_banner = lambda *_: None
 app = Flask('')
 
@@ -8,9 +9,27 @@ app = Flask('')
 def home():
     return "Hello. I am alive!"
 
-def run():
-  app.run(host='0.0.0.0',port=8080)
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
 
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
+
+def run():
+  isRunning = False
+  while isRunning == False:
+    try:
+      app.run(host='0.0.0.0',port=8080)
+      isRunning = True
+    except OSError as e:
+      print(e)
+      resp = requests.post('https://Kur0bot.jericjanjan.repl.co/shutdown')
+      
 def keep_alive():
     t = Thread(target=run)
     t.start()
