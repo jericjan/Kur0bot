a06aeb69b1a3c0f8155e95088673e1aee9df5ae7
Jeric Jan Jan <38642145+jericjan@users.noreply.github.com>
2022-04-16 08:46:31 +0000

    rs

--- modules/others/resize.py
+++ modules/others/resize.py
@@ -87,6 +87,40 @@ class Resize(commands.Cog):
         bruh.close()
         await message.delete()
 
+    @commands.command()
+    async def rs(self, ctx, link=None):
+        if link == None:
+            print(ctx.message.attachments)  # a list
+            print(ctx.message.reference)
+            if ctx.message.attachments:  # message has images
+                print("is attachment")
+                link = ctx.message.attachments[0].url
+            elif ctx.message.reference is not None:  # message is replying
+                print("is reply")
+                id = ctx.message.reference.message_id
+                msg = await ctx.channel.fetch_message(id)
+                if msg.attachments:  # if replied has image
+                    link = msg.attachments[0].url
+                elif msg.embeds:  # if replied has link
+                    link = msg.embeds[0].url
+
+                # print("embmeds: {0}".format(msg.embeds))
+                # if re.search(r'http.*\bpng\b|http.*\bjpg\b|http.*\bjpeg\b',msg.content):
+                #   link = re.search(r'http.*\bpng\b|http.*\bjpg\b|http.*\bjpeg\b',msg.content)[0]
+                # link= msg.attachments[0].url
+                else:
+                    await ctx.send("the message you replied to has no image, baka!")
+            else:
+                await ctx.send("you did something wrong. brug. try again.")
+                return
+            print(link)
+        message = await ctx.send("Resizing...")
+        bruh = await self.foo(link, 1600, 720)
+        bruh.seek(0)
+        filename = link.split("/")[-1]
+        await ctx.send(file=disnake.File(bruh, filename=filename))
+        bruh.close()
+        await message.delete()
 
 def setup(client):
     client.add_cog(Resize(client))
