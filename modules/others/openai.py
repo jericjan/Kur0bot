921fc26015e87d580fd362681efcc6b56b37066b
jericjan <cantilfrederick@gmail.com>
2025-06-29 18:22:16 +0800

    karaoke and openai type hinted

--- modules/others/openai.py
+++ modules/others/openai.py
@@ -1,31 +1,36 @@
 from importlib.metadata import version
-from typing import Any
+from typing import Any, cast
 
 import g4f  # type: ignore
-import nest_asyncio  # type: ignore
+import nest_asyncio  # type: ignore (remind me why i added this?)
 from disnake.ext import commands
 from g4f.client import Client  # type: ignore
-
+import g4f.Provider  # type: ignore
+from g4f.client.types import ChatCompletion  # type: ignore
 
 class OpenAI(commands.Cog):
     def __init__(self, client: commands.Bot):
         print(f"g4f version is {version('g4f')}")
         self.client = client
-        nest_asyncio.apply()
+        nest_asyncio.apply()  # type: ignore
 
     def prompt(self, msg: str) -> str:
         client = Client()
-        response = client.chat.completions.create(
-            model="meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo",
-            provider=g4f.Provider.DeepInfraChat,
-            messages=[{"role": "user", "content": msg}],
+        response = cast(
+            ChatCompletion,
+            client.chat.completions.create(  # pyright: ignore[reportUnknownMemberType, reportAssignmentType]
+                model="meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo",
+                provider=g4f.Provider.DeepInfraChat,
+                messages=[{"role": "user", "content": msg}],
+            )
         )
-        return response.choices[0].message.content
+        res = response.choices[0].message.content
+        return res if res else "No response."
 
     @commands.command()
-    async def gpt(self, ctx: commands.Context[Any], *, msg):
+    async def gpt(self, ctx: commands.Context[Any], *, msg: str):
 
-        def split_long_string(long_string, chunk_size=2000):
+        def split_long_string(long_string: str, chunk_size: int =2000):
             return [
                 long_string[i : i + chunk_size]
                 for i in range(0, len(long_string), chunk_size)
